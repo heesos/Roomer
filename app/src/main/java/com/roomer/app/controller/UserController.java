d3d4cb8c26280742cb470c3f7700fe91b268d1aa
Milosz <marzec1310@gmail.com>
2023-03-22 23:40:48 +0100

    Added javaDoc, somemethods to controller and service, sequence generator working

--- app/src/main/java/com/roomer/app/controller/UserController.java
+++ app/src/main/java/com/roomer/app/controller/UserController.java
@@ -3,27 +3,78 @@ package com.roomer.app.controller;
 import com.roomer.app.domain.User;
 import com.roomer.app.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Rest Controller for User entity
+ * @author milosz.marzec
+ */
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/user")
 public class UserController {
     private UserService userService;
 
+    /**
+     * Creates a new user
+     * @param user user object with all the information
+     * @return HTTP response with new created User
+     */
     @PostMapping("/add")
-    public void addUser(@RequestBody User user) {
-        userService.saveUser(user);
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.saveUser(user);
+        return ResponseEntity.ok(newUser);
     }
 
+    /**
+     * Fetches a list of Users from database
+     * @return list of Users
+     */
     @GetMapping("/all")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> userList = userService.getAllUsers();
-        return new ResponseEntity<>(userList, HttpStatus.OK);
+        return ResponseEntity.ok(userList);
     }
+
+    /**
+     * Fetches User from database by id
+     * @param id id of user to be found
+     * @return User object
+     */
+    @GetMapping("/1")
+    public ResponseEntity<User> getUser() {
+        User user = userService.getUserById(52);
+        System.out.println("controller called");
+        System.out.println(user.getId());
+        return ResponseEntity.ok(user);
+    }
+
+    /**
+     * Deletes a User from database
+     * @param id id of a User to delete
+     * @return HTTP Status
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Updates an existing User
+     * @param user User with new data
+     * @param id id of a User to update
+     * @return User object with new data which was saved to database
+     */
+    @PostMapping("/{id}")
+    public ResponseEntity<Long> updateUser(@RequestBody User user, @PathVariable("id") Long id) {
+        userService.updateUser(id, user);
+        return ResponseEntity.ok(id);
+    }
+
+    //TODO:
+    // - damn all methods apart from getAll are not working idk why
 }
--- app/src/main/java/com/roomer/app/service/UserService.java
+++ app/src/main/java/com/roomer/app/service/UserService.java
@@ -7,18 +7,73 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service class responsible for business logic when performing database operations with User entity
+ *
+ * @author milosz.marzec
+ */
 @Service
 @AllArgsConstructor
 public class UserService {
 
     private UserRepository userRepository;
 
-    public long saveUser(User user) {
+    /**
+     * Saves a new User to the database
+     *
+     * @param user User entity
+     * @return created User in database
+     */
+    public User saveUser(User user) {
         userRepository.save(user);
-        return user.getId();
+        return user;
     }
 
+    /**
+     * Searches for all Users in database
+     *
+     * @return list of Users
+     */
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    /**
+     * Removes User from database
+     *
+     * @param id id of User to remove
+     */
+
+    public void removeUserById(long id) {
+        userRepository.deleteById(id);
+    }
+
+    /**
+     * Gets User from database by id
+     *
+     * @param id User id
+     * @return User object
+     */
+    public User getUserById(long id) {
+        System.out.println("service called");
+        return userRepository.getReferenceById(id);
+    }
+
+    /**
+     * Updates an existing User in the database
+     *
+     * @param userId  id used to search User to update
+     * @param newUser User with the new data
+     * @return id of an updated User object
+     */
+    public long updateUser(long userId, User newUser) {
+        User userToUpdate = userRepository.getReferenceById(userId);
+        userToUpdate.setName(newUser.getName());
+        userToUpdate.setDescription(newUser.getDescription());
+        userToUpdate.setDateOfBirth(newUser.getDateOfBirth());
+
+        userRepository.save(userToUpdate);
+
+        return userId;
     }
 }
